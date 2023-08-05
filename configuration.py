b37ea6c559e25309370d5b942d451863edc2dc55
Irina <irinavylegzhanina@rambler.ru>
2023-08-05 14:41:42 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://a613aa04-e7ea-4219-b34b-917e4ea0e6cf.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
